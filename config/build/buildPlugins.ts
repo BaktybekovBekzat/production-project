937209691d7016a0ef6f28c4d3fdd683f434468f
hhsqlp <hhsqlp@gmail.com>
2023-08-26 16:58:14 +0600

    added bundle analyèr

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from 'webpack'
 import HTMLWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { IBuildOptions } from './types'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins(options: IBuildOptions): WebpackPluginInstance[] {
 	return [
@@ -15,6 +16,9 @@ export function buildPlugins(options: IBuildOptions): WebpackPluginInstance[] {
 		}),
 		new DefinePlugin({
 			__IS_DEV__: options.isDev,
+		}),
+		new BundleAnalyzerPlugin({
+			openAnalyzer: false,
 		}),
 	]
 }
