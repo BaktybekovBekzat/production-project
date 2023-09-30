c5df573de5d1e02cfd7df160e334a565cc9aef08
hhsqlp <hhsqlp@gmail.com>
2023-09-30 23:08:52 +0600

    fixed stylelint error

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
-import styles from './LoginModal.module.scss'
 import { Modal } from 'shared/ui/Modal/Modal'
 import { LoginForm } from '../LoginForm/LoginForm'
 
@@ -17,7 +16,7 @@ export const LoginModal: FC<IProps> = (props) => {
 		<Modal
 			isOpen={isOpen}
 			onClose={onClose}
-			className={classNames(styles.LoginModal, {}, [className])}
+			className={classNames('', {}, [className])}
 			lazy
 		>
 			<LoginForm />
